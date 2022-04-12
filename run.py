eb1b0b01686790f4afcd4dfd617e404a08d71c71
Gabarron <>
2022-04-12 22:00:15 +0100

    Implement Start Menu and present user the options

--- run.py
+++ run.py
@@ -12,24 +12,81 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('superb-payroll')
 
-def get_sales_data():
+def start_menu():
     """
-    Get sales figures input from the user.
+    Start Menu to give user two options: 1- Add new employee  2- Search for employee ID
+    Run a while loop to collect a valid string of data from the user
+    Run validate_option function to option entry is correct. 
+    The loop will repeatedly request data, until it is valid.
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True: 
+        print("Please choose one option: \n 1- Add new employee \n 2- Search for employee ID\n")
+        user_option = input("Enter number 1 for option 1 or number 2 for option 2\n")
 
-    data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+        if validate_option(user_option):
+            break
 
-    data = ["0030", "Maria", "09/10/2009", "09/10/1990", "80000", "Sale"]
-    employee_worksheet = SHEET.worksheet("employee")
-    employee_worksheet.append_row(data)
+    if user_option == '1':
+        new_entry()
+    elif user_option == '2':
+        search_employeeID()
 
-    cell = employee_worksheet.find("0030")
+def validate_option(user_option):
+    """
+    Validate if user_option is equal to 1 or 2.
+    Implementing a try except to validate user_option
+    """
+    try:
+        if user_option != '1' and user_option != '2':
+            raise ValueError(
+                f"Please select options number 1 or 2, you provided {user_option}"
+            )
 
-    print(f"Found something at {cell.row}{cell.col}")   
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+    return True
 
 
-get_sales_data()
+def new_entry():
+    """
+    Add a new employee to the system. Write a new employee row on google sheets
+    """
+    print("Add employee")
+
+def search_employeeID():
+    """
+    Search for employee ID.
+    Ask the user for employee ID and search through employee list to find a match.
+    """
+    print("Search employee ID")
+
+# def get_data():
+#     """
+#     Get sales figures input from the user.
+#     """
+#     print("Please enter sales data from the last market.")
+#     print("Data should be six numbers, separated by commas.")
+#     print("Example: 10,20,30,40,50,60\n")
+
+#     data = ["0030", "Maria", "09/10/2009", "09/10/1990", "80000", "Sales"]
+#     employee_worksheet = SHEET.worksheet("employee")
+#     # employee_worksheet.append_row(data)
+
+    
+#     cell = employee_worksheet.find("0030")
+#     list_of_dicts = employee_worksheet.get_all_records()
+#     print(list_of_dicts)
+#     value = employee_worksheet.cell(len(list_of_dicts)+1 , 1).value
+#     print(int(value)+1)
+
+#     if cell == None:
+#         print("Nothing equal, please go ahead")
+#         # values_list = employee_worksheet.row_values(cell.row)
+#         # print(values_list)
+
+
+
+
+start_menu()
+# get_data()
